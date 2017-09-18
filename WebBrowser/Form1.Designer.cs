6b506128d4f38bb7f268e18d3caa0ec68469ba61
shenqiangbin <shenqiangbin@163.com>
2017-09-18 23:24:54 +0800

    add

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(495, 2);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "前往";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(2, 29);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1289, 690);
+            this.webBrowser.TabIndex = 2;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser_Navigated);
+            this.webBrowser.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser_NewWindow);
+            this.webBrowser.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.webBrowser_ControlAdded);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(58, 3);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(432, 21);
+            this.txtUrl.TabIndex = 3;
+            this.txtUrl.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "网址：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1291, 750);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.txtUrl.Text = "http://saishi.cnki.net/exam/Exam/Start/sj2afbb1c3-a9ac-4106-b396-010c73133b2b";
+            this.txtUrl.Text = "http://saishi.cnki.net/passport/Account/Login";
+            //this.txtUrl.Text = "http://127.0.0.1/abc.html";
+            this.webBrowser.Navigate(this.txtUrl.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.webBrowser.Navigate(this.txtUrl.Text);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string tigan = "";
+            string answer = "";
+            HtmlElementCollection divs = this.webBrowser.Document.GetElementsByTagName("div");
+            foreach (HtmlElement item in divs)
+            {
+                if (item == null || item.InnerHtml == null)
+                    continue;
+                var str = item.OuterHtml.Replace(item.InnerHtml, "");
+                str = str.Replace("\"", "");
+                var classVal = str.Substring(str.IndexOf("class=") + 6, str.IndexOf("><") - (str.IndexOf("class=") + 6));
+                if (!string.IsNullOrEmpty(classVal))
+                {
+                    if (string.IsNullOrEmpty(answer) && classVal == "tigan")
+                    {
+                        tigan = item.InnerHtml;
+                        answer = GetAnswer(tigan);
+                    }
+                    if (classVal == "iradio_square-green")
+                    {
+                        var inputEle = item.FirstChild;
+                        var inputEleVal = inputEle.GetAttribute("value");
+                        if (inputEle != null && answer.Contains(inputEleVal))
+                        {
+                            Thread.Sleep(1000);
+                            MessageBox.Show("选" + inputEleVal);
+                            inputEle.InvokeMember("click");                           
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private string GetAnswer(string tigan)
+        {
+            //if (tigan == "成语“闻鸡起舞”、“中流击楫”是出自哪个古代将领的故事？")
+            //{
+            //    return "A";
+            //}
+            //else
+            //{
+            //    return "B";
+            //}\
+            string[] arr = new[] { "A", "B", "C", "D" };
+            return arr[new Random().Next(0, 4)];
+        }
+
+        //http://blog.csdn.net/taoerchun/article/details/49782739
+        private void InjectScript()
+        {
+            HtmlElement ele = this.webBrowser.Document.CreateElement("script");
+            ele.SetAttribute("type", "text/javascript");
+            ele.SetAttribute("text", @"
+");
+
+            // 注入.
+            webBrowser.Document.Body.AppendChild(ele);
+            object result = webBrowser.Document.InvokeScript("GetPictureData");
+            
+
+            //MessageBox.Show(buff.ToString());
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login()
+        {
+            var usernameEle = GetElementByName("input", "user-name");
+            usernameEle.SetAttribute("value", "15201106926");
+
+            var pwd = GetElementByName("input", "password");
+            pwd.SetAttribute("value", "woshishen");
+
+            var loginBtn = webBrowser.Document.Window.Frames[0].Document.GetElementById("dologin");
+            //loginBtn.InvokeMember("click");
+
+        }
+
+        private HtmlElement GetElementByName(string type, string name)
+        {
+            HtmlElementCollection inputs = webBrowser.Document.Window.Frames[0].Document.GetElementsByTagName(type);
+            foreach (HtmlElement item in inputs)
+            {
+                if (item.Name.ToLower() == name.ToLower())
+                    return item;
+            }
+            return null;
+        }
+
+        private void btnFillData_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            var url = e.Url;
+            this.txtUrl.Text = url.ToString();
+            if (url.ToString() == "http://saishi.cnki.net/")
+            {
+                //var url2 = "http://saishi.cnki.net/exam/Exam/Answer/kjf0e12af0-9870-11e7-8221-1866dae6f470";
+                //url2 = "http://127.0.0.1/abc.html";
+                //this.webBrowser.Navigate(url2);
+            }
+            //this.webBrowser.Navigate("http://saishi.cnki.net/exam/Exam/Start/sj2afbb1c3-a9ac-4106-b396-010c73133b2b");
+        }
+
+        private void webBrowser_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;//取消打开新窗口动作
+            try
+            {
+                //获取要打开的新窗口链接地址
+                string url = this.webBrowser.Document.ActiveElement.GetAttribute("href");
+                this.webBrowser.Navigate(url);
+            }
+            catch
+            {
+            }
+        }
+
+        private void webBrowser_ControlAdded(object sender, ControlEventArgs e)
+        {
+            
+        }
+    }
+
+    //private void btnInput_Click(object sender, EventArgs e)
+    //{
+    //    var input = this.webBrowser1.Document.GetElementById("kw");
+    //    input.SetAttribute("value", "好例子网");
+    //}
+
+    //private void btnOper_Click(object sender, EventArgs e)
+    //{
+    //    var button = this.webBrowser1.Document.GetElementById("su");
+    //    button.InvokeMember("click");
+    //}
+}
